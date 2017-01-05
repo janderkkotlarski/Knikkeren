ab85e9c98b283d005d5c3805202ef6f381369969
janderkkotlarski <j.d.kotlarski@gmx.com>
2017-01-05 21:16:26 +0100

    WIP

--- Knikkeren/window_looper.h
+++ Knikkeren/window_looper.h
@@ -6,17 +6,19 @@
 
 class Window_Looper
 {
-    const std::string m_name;
+    const std::string m_name{"Knikkeren"};
 
     const sf::Vector2f m_dims;
 
-    const float m_fps;
-    const float m_frame;
+    const float m_fps{60}; // Zero Punctuation Stereotype
+    const float m_frame{1/m_fps};
 
-    const sf::Color m_background;
-    const sf::Color m_filler;
+    const sf::Color m_background{127, 63, 31};
+    const sf::Color m_filler{223, 127, 95};
 
+public:
 
+    Window_Looper()
 
 };
 
